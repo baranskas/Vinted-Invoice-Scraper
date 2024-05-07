b08597fd587506b46ad8b562130af52faa7eb5a0
DevCache1 <martynasbaranskas@gmail.com>
2024-05-07 11:51:51 +0300

    V0.0.1 Functionality Implemented
    Main functionality implemented + Added screenshot of graph. Edited README.md

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Vinted-Invoice-Scraper
- Scrape your Vinted total sold & purchased value data.
+# Vinted Invoice Scraper
+Because I'm a huge second-hand store fanatic, I wanted to see for how much value I sold or bought clothing.
+Uses BeautifulSoup to scrape the .html file Vinted sends you.
+
+### How to use
+Download your own data from your Vinted user settings, put it in the same folder as the script (index.html from the orders folder inside the .zip)
+
+### Example graph
+![](Figure_1.png)
---
+++ main.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+from collections import defaultdict
+from datetime import datetime
+
+with open("index.html", "r", encoding="utf-8") as file:
+    html_content = file.read()
+
+soup = BeautifulSoup(html_content, "html.parser")
+
+cells = soup.find_all("div", class_="cell")
+
+daily_values = defaultdict(lambda: {"sold": 0, "purchased": 0})
+
+for cell in cells:
+    buyer_span = cell.find("span", itemprop="buyer")
+    order_value_span = cell.find("span", itemprop="order_value")
+    order_purchased_span = cell.find("span", itemprop="order_purchased")
+    
+    if buyer_span and order_value_span and order_purchased_span:
+        buyer = buyer_span.get_text(strip=True)
+        order_value = float(order_value_span.get_text(strip=True))
+        order_purchased = datetime.strptime(order_purchased_span.get_text(strip=True), "%Y-%m-%d %H:%M:%S %z")
+        
+        day_key = order_purchased.strftime("%Y-%m-%d")
+        daily_values[day_key]["purchased" if buyer == "baranskas" else "sold"] += order_value
+
+dates = sorted(daily_values.keys())
+sold_values = [daily_values[date]["sold"] for date in dates]
+purchased_values = [daily_values[date]["purchased"] for date in dates]
+
+plt.figure(figsize=(14, 6))
+plt.bar(dates, sold_values, width=0.4, label="Sold", align="center", color="blue")
+plt.bar(dates, purchased_values, width=0.4, label="Purchased", align="edge", color="orange")
+plt.xlabel("Date")
+plt.ylabel("Value")
+plt.title("Daily Sold and Purchased Items Value")
+plt.xticks(rotation=90, fontsize=8)
+plt.legend()
+plt.grid(True)
+plt.tight_layout()
+plt.show()
